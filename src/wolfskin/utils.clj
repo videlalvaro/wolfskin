8127df6bdbcca66ad9ccce1e532749777bfb9967
Alvaro Videla <videlalvaro@gmail.com>
2011-10-10 17:43:03 +0200

    utility functions to run queries on a remote repository

---
+++ src/wolfskin/utils.clj
@@ -0,0 +1,16 @@
+(ns wolfskin.utils
+  (:require [cljcr.core :as jcr]
+            [cljcr.rmi :as rmi]))
+
+(defn run-query [url username password ws-name qtype statement]
+  (let [repo (rmi/get-repository url)
+        credentials (rmi/get-credentials username password)
+        session (rmi/get-session repo credentials ws-name)
+        workspace (rmi/get-workspace session)
+        qm (rmi/get-query-manager workspace)
+        language (rmi/get-query-type qtype)]
+    (rmi/get-nodes (rmi/execute (rmi/create-query qm statement language)))))
+
+(defn node-properties [props]
+  (map #(jcr/get-property-map (jcr/properties %)) props))
+
