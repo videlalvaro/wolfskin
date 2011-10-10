0c32a95fe8b3ea9482cc5b831e54de1fa6d84ed9
Alvaro Videla <videlalvaro@gmail.com>
2011-10-10 17:42:13 +0200

    adds command-line dependency

--- project.clj
+++ project.clj
@@ -2,10 +2,13 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure.contrib/zip-filter "1.3.0-SNAPSHOT"]
+                 [org.clojure.contrib/command-line "1.3.0-SNAPSHOT"]
                  [javax.jcr/jcr "2.0"]
                  [org.apache.jackrabbit/jackrabbit-core "2.2.9"]
                  [org.apache.jackrabbit/jackrabbit-jcr-rmi "2.2.8"]
                  [cljcr/cljcr "1.0.0-SNAPSHOT"]
                  [org.slf4j/slf4j-log4j12 "1.5.8"]
                  ;[org.markdownj/markdownj "0.3.0-1.0.2b4"]
-                 ])
+                 ]
+  :run-aliases {:cli wolfskin.cli}
+  :jvm-opts ["-Dfile.encoding=utf-8"])
