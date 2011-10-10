e306dd116f1f77fe3e45a5e738d2fb34ca564ccf
Alvaro Videla <videlalvaro@gmail.com>
2011-10-10 17:43:42 +0200

    CLI utility to execute queries remotely

---
+++ src/wolfskin/cli.clj
@@ -0,0 +1,23 @@
+(ns wolfskin.cli
+  (:require [clojure.pprint :as pprint]
+            [clojure.contrib.command-line :as ccl]
+            [wolfskin.utils :as utils]
+            ))
+
+(defn -main [& args]
+  (ccl/with-command-line args
+    "Command line demo"
+    [[url "Repository URL" "http://localhost:8080/rmi"]
+     [username u "User Name" "admin"]
+     [password p "User password", "admin"]
+     [workspace w "JCR Workspace"]
+     [sql? "Sets query as JCR_SQL2" true]
+     [xpath? "Sets query as XPATH"]
+     [query q "Query to execute"]]
+    (let [qtype (if sql? :sql :xpath)
+          nodes (utils/run-query url username password workspace qtype query)
+          ]
+      (pprint/pprint (utils/node-properties nodes)))))
+
+
+
